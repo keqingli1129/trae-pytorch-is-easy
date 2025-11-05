eb52fd2a31ab69b97f5537acbf8ef83787557b68
Keqing Li <keqingli1129@gmail.com>
2025-11-04 20:57:33 -0600

    gpt block

---
+++ gptblock.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+
+class Block(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.ln_1 = nn.LayerNorm(config.n_embd)
+        self.attn = CausalSelfAttention(config)
+        self.ln_2 = nn.LayerNorm(config.n_embd)
+        self.mlp = MLP(config)
+        self.config = config
+
+    def forward(self, x):
+        x = x + self.attn(self.ln_1(x))
+        x = x + self.mlp(self.ln_2(x))
+        return x
